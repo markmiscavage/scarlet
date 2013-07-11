8f69b134cfa322d234e2af587337fa2d5be727e3
Christian Harden <christian.harden@gmail.com>
2013-07-10 17:20:18 -0700

    mapping scarlet/source/scarlet to source/scarlet

--- scarlet/cms/settings.py
+++ scarlet/cms/settings.py
@@ -1,4 +1,11 @@
+import os.path
+
 from django.conf import settings as django_settings
 
 INTERNAL_TAG_HANDLER = getattr(django_settings, 'SCARLET_INTERNAL_TAG_HANDLER',
                              '.taggit_handler')
+
+
+PROJECT_ROOT = os.path.abspath(os.path.join(os.path.dirname(__file__)))
+SCARLET_SOURCE_ROOT = os.path.join(PROJECT_ROOT, 'source')
+django_settings.STATICFILES_DIRS += (SCARLET_SOURCE_ROOT,)
