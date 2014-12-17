f8f5d564c3d2f55a00cf03e151bebd4c87b38ea2
anna yovandich <anna.yovandich@ff0000.com>
2014-12-17 14:43:42 -0800

    add insertLink as custom cmd to override default

--- scarlet/cms/static/scarlet/js/admin/modules/wysiwyg/commands/commands.js
+++ scarlet/cms/static/scarlet/js/admin/modules/wysiwyg/commands/commands.js
@@ -2,11 +2,13 @@ define(
 	[
 		"wysihtml5",
 		"./insertMedia",
-		"./insertAnnotation"
+		"./insertAnnotation",
+		"./insertLink"
 	],
-	function (wysihtml5, insertMedia, insertAnnotation) {
+	function (wysihtml5, insertMedia, insertAnnotation, insertLink) {
 
 		// Extend list of wysiwyg commands here.
 		wysihtml5.commands.insertMedia = insertMedia;
 		wysihtml5.commands.insertAnnotation = insertAnnotation;
+		wysihtml5.commands.insertLink = insertLink;
 	});
