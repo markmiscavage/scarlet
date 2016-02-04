c3cb9ef333538a646bfb6d1932f8233289a9f480
will <will.mclafferty@ff0000.com>
2016-02-03 19:11:34 -0800

    fixing some bad imports

--- scarlet/accounts/management/__init__.py
+++ scarlet/accounts/management/__init__.py
@@ -1,4 +1,4 @@
-from django.db.models.signals import post_syncdb
+from django.db.models.signals import post_migrate
 from django.contrib import auth
 from django.contrib.auth.models import Group
 from django.contrib.auth import models as auth_app
@@ -9,7 +9,6 @@ from .. import settings
 
 
 def ensure_groups_sync(app, created_models, verbosity, **kwargs):
-
     for group in settings.BASE_GROUPS:
         Group.objects.get_or_create(name=group)
 
@@ -18,8 +17,5 @@ def ensure_groups(sender=None, **kwargs):
     for group in settings.BASE_GROUPS:
         Group.objects.get_or_create(name=group)
 
-if DJANGO_VERSION < (1, 7):
-    post_syncdb.connect(ensure_groups_sync, sender=auth.models,
-                    dispatch_uid='ensure_groups')
-else:
-    post_migrate.connect(ensure_groups, sender=auth_app, dispatch_uid='ensure_groups')
+
+post_migrate.connect(ensure_groups, sender=auth_app, dispatch_uid='ensure_groups')
