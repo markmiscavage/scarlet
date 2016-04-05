487e4cbbb35eb1f73f138ea892fffcde641468de
anna yovandich <anna.yovandich@ff0000.com>
2016-04-05 11:24:17 -0700

    added entry point (init.js) and stub App component

---
+++ scarlet/cms/static/scarlet/source/components/App.js
@@ -0,0 +1,15 @@
+import React, { PropTypes, Component } from 'react'
+
+class App extends Component {
+	constructor(props) {
+		super(props)
+	}
+
+	render() {
+		return (
+			<div>React has taken over.</div>
+		)
+	}
+}
+
+export default App
