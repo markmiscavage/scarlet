64a6adb283da9d9853bcda171cabc72120dfa8b0
anna yovandich <anna.yovandich@ff0000.com>
2016-04-14 15:32:28 -0700

    gutted old AutoSlug.spec (needs definition)

--- scarlet/cms/static/scarlet/source/js/components/autoSlug/AutoSlug.spec.js
+++ scarlet/cms/static/scarlet/source/js/components/autoSlug/AutoSlug.spec.js
@@ -1,37 +1,15 @@
 'use strict'
-import React from 'react'
-import AutoSlug from './AutoSlug'
-import TestUtils from 'react-addons-test-utils'
+import AutoSlug from './autoSlug/AutoSlug'
 
+describe('AutoSlug.js spec', function () {
+  const view
 
-describe('AutoSlug Component', function() {
+  beforeEach(function () {
+  })
 
-    let renderer = TestUtils.createRenderer()
-
-    it('should render a thing', function() {
-        let props = {
-          sourceNode: document.createElement('input'),
-          slugNode: document.createElement('input'),
-          label: labelNode.textContent,
-          fieldAttributes: {
-            id: dataNode.id,
-            name: dataNode.name,
-            maxLength: dataNode.maxlength,
-            type: dataNode.type,
-            className: dataNode.class
-          },
-          labelAttributes: {
-            className: labelNode.class
-          }
-        }
-        renderer.render(<AutoSlug {...props} />)
-
-        let autoSlugNode = renderer.getRenderOutput()
-
-        expect(autoSlugNode.type).toBe('div')
-        TestUtils.Simulate.change(autoSlugNode)
-        TestUtils.Simulate.keyDown(autoSlugNode, {key: 'Enter', keyCode: 13, which: 13})
-        console.log(JSON.stringify(autoSlugNode))
+  describe('Rendering', function () {
+    it ('Receives correct HTML', function() {
     })
+  })
 })
 
