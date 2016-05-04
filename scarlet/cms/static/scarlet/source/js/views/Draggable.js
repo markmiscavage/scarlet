ab17887e5e09f9ed41fcce51d102861615f94b54
Justin Marshall <justin.marshall@ff0000.com>
2016-05-03 17:57:38 -0700

    fixed legacy formatting issues

--- scarlet/cms/static/scarlet/source/js/views/Draggable.js
+++ scarlet/cms/static/scarlet/source/js/views/Draggable.js
@@ -5,13 +5,13 @@ class Draggable {
 	}
 
 	init () {
-		let draggable = this.$el.find("tr[draggable]").parents("tbody")
+		let draggable = this.$el.find('tr[draggable]').parents('tbody')
 
 		draggable.sortable({
 			stop: function (e, ui) {
-				let inputs = $(ui.item.context.offsetParent).find(":text")
+				let inputs = $(ui.item.context.offsetParent).find(':text')
 
-				for (var i = 0; i < inputs.length; i++) {
+				for (var i = 0, len = inputs.length; i < len; i++) {
 					inputs[i].value = i + 1
 				}
 			}
