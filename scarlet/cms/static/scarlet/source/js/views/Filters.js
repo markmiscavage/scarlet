5c66c473c53242d7b6e4fc65e1ce6732ad5e7a05
anna yovandich <anna.yovandich@ff0000.com>
2016-05-04 14:31:39 -0700

    prefixed jQuery object with $

--- scarlet/cms/static/scarlet/source/js/views/Filters.js
+++ scarlet/cms/static/scarlet/source/js/views/Filters.js
@@ -12,11 +12,11 @@ const Filters = View.extend({
 
   closeSiblings: function (e) {
     this.$dropDowns.not($(e.currentTarget)).each(function (i) {
-      var details = $(this).parent()
-      if (details.attr('open') !== undefined) {
-        details.removeAttr('open')
-      } else if (details.hasClass('open')) {
-        details.removeClass('open').addClass('closed').attr('data-open', 'closed')
+      var $details = $(this).parent()
+      if ($details.attr('open') !== undefined) {
+        $details.removeAttr('open')
+      } else if ($details.hasClass('open')) {
+        $details.removeClass('open').addClass('closed').attr('data-open', 'closed')
       }
     })
   }
