2331db79ab94a527a2b9cc12edc31d608f1f9c71
anna yovandich <anna.yovandich@ff0000.com>
2016-11-03 00:31:07 -0700

    moved font-awesome.scss to /vendor directory; updated config path

--- webpack-font-awesome-sass.config.js
+++ webpack-font-awesome-sass.config.js
@@ -1,14 +1,14 @@
 module.exports = {
-  fontAwesomeCustomizations: './scarlet/cms/static/scarlet/source/stylesheets/base/_font-awesome.config.scss',
- 
+  fontAwesomeCustomizations: './scarlet/cms/static/scarlet/source/stylesheets/vendor/_font-awesome.scss',
+
  	styleLoader: require('extract-text-webpack-plugin').extract('style-loader', 'css-loader!sass-loader'),
 
   styles: {
     'mixins': true,
- 
+
     'core': true,
     'icons': true,
- 
+
     'larger': true,
     'path': true,
   }
