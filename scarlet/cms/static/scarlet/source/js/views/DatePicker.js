cd6d8764e755a9ca36ce6bfc40ce712b41068caf
anna yovandich <anna.yovandich@ff0000.com>
2016-11-02 17:24:37 -0700

    updated naming convention and path

--- scarlet/cms/static/scarlet/source/js/views/DatePicker.js
+++ scarlet/cms/static/scarlet/source/js/views/DatePicker.js
@@ -1,6 +1,6 @@
 import { View } from 'backbone'
-import datepicker from 'jquery-ui/datepicker'
-import '../../stylesheets/views/datetimepicker.scss'
+import datepicker from 'jquery-ui/ui/widgets/datepicker'
+import '../../stylesheets/views/date-picker.scss'
 
 const DatePicker = View.extend({
   render: function() {
