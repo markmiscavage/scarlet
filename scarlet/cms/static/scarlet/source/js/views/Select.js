4c7aea1cf3a809952b27b5d089b075cee33d2cd6
anna yovandich <anna.yovandich@ff0000.com>
2016-11-30 11:06:45 -0800

    styled dashboard filter control

--- scarlet/cms/static/scarlet/source/js/views/Select.js
+++ scarlet/cms/static/scarlet/source/js/views/Select.js
@@ -4,7 +4,16 @@ import selectize  from 'selectize'
 const Select = View.extend({
   render: function () {
     this.selectize = this.$el.selectize({
-    	selectOnTab: true
+    	selectOnTab: true,
+      onChange: function () {
+        const $parentForm = this.$input.parents('form')
+
+        // NOTE: hacky workaround to submit filter value on dashboard...
+        // ...could not modify form field (select) to reflect this config
+        if ($parentForm && $parentForm.hasClass('js-submit-on-change')) {
+          this.$input.parents('form').submit()
+        }
+      }
     })
   }
 })
