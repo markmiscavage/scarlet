42faf805b151cc481869c3bb60630766db8585de
Arturo Fernandez <arturo.fernandez@ff0000.com>
2017-07-12 11:11:28 -0700

    Changing color when user click on Title field

--- scarlet/hotspots/cms_forms.py
+++ scarlet/hotspots/cms_forms.py
@@ -4,6 +4,10 @@ from . import models
 
 
 class HotSpotForm(forms.ModelForm):
+    def __init__(self, *args, **kwargs):
+        super(HotSpotForm, self).__init__(*args, **kwargs)
+        self.fields.get('order').widget.attrs['class'] = 'order-inp'
+
     class Meta:
         model = models.HotSpot
         fields = ('x_cord', 'y_cord', 'order', 'icon', 'label', 'text', 'image', 'video_json', )
