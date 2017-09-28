a3e68682c4155fbdb4e1279237c0b7c088f948b6
Arturo Fernandez <arturo.fernandez@ff0000.com>
2017-09-28 14:23:08 +0200

    Fixing dump_versioned command for Django 1.11

--- scarlet/versioning/management/commands/dump_versioned.py
+++ scarlet/versioning/management/commands/dump_versioned.py
@@ -4,9 +4,9 @@ from django.core.management.commands import dumpdata
 class Command(dumpdata.Command):
 
     def handle(self, *app_labels, **options):
-        from django.db.models import get_models
+        from django.apps import apps
         exclude = options.get('exclude', [])
-        for m in get_models():
+        for m in apps.get_models():
             if getattr(m._meta, '_is_view', False):
                 l = "%s.%s" % (m._meta.app_label, m._meta.object_name)
                 exclude.append(l)
