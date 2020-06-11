e67c4dcf269830a3f56b1e037776cd72bbdaf95b
markmiscavage <mark.miscavage@wonderful.com>
2020-06-10 17:44:25 -0700

    removing leftover ipython statement

--- scarlet/cms/sites.py
+++ scarlet/cms/sites.py
@@ -150,7 +150,6 @@ class AdminSite(object):
 
         Currently checks that the user is active and a staff member.
         """
-        #from IPython import embed;embed()
         return request.user.is_active and request.user.is_staff
 
     def admin_view(self, view, cacheable=False):
