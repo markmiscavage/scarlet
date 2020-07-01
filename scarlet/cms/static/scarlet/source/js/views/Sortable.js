4180d1049a16e6e2f9a2e2f4190e3358939091dd
Ugo Le Prudhomme <ugo@contact.fr>
2020-06-30 17:12:33 -0700

    SCMS-77 - Fix Sortable component to be able to save re-ordered index

--- scarlet/cms/static/scarlet/source/js/views/Sortable.js
+++ scarlet/cms/static/scarlet/source/js/views/Sortable.js
@@ -3,11 +3,25 @@ import SortableJS from 'sortablejs';
 
 const Sortable = View.extend({
 
+  initialize(){
+    this.currentPage = this.$el.attr('data-current-page');
+    this.perPage = this.$el.attr('data-per-page');
+  },
+
   render() {
     SortableJS.create(this.el,{
       animation: 150,
+      onEnd: () => {
+        this.setIndex()
+      }
     });
   },
+
+  setIndex(){
+    $.each(this.$el.find('tr .orderfield'), (index,item) => {
+      $(item).val((this.currentPage - 1) * this.perPage + index);
+    });
+  }
 });
 
 export default Sortable;
